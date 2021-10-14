ab2b8d03e54ec38bb82d243ad9ca16075223f320
agussanguesa32 <agussanguesa@gmail.com>
2021-10-14 09:12:11 -0300

    Seteo del proyecto en intelliJ

---
+++ src/main/java/Email.java
@@ -0,0 +1,14 @@
+package info3.parcial2;
+
+/**
+ * Clase con los datos de un email. Puede tener mas propiedades o metodos
+ */
+class Email {
+
+    public long id;
+    public String from;    // Remitente del mail
+    public String to;      // destinatario del mail
+    public String date;    // Fecha de envio
+    public String subject; // Asunto del mail
+    public String content; // Contenido del mail.
+}
